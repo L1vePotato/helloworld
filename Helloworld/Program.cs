0499f2662637bec345df41ba4181b4630c4921d3
L1vePotato <dudarev.sasha2018@gmail.com>
2022-02-28 15:44:12 +0500

    first project

---
+++ Helloworld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld!");
+            Console.WriteLine("Oh, Hi, Mark!");
+            Console.ReadLine();
+        }
+    }
+}
